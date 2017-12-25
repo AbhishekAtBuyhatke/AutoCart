c3601e3866931e92e7595d4b8ba877f12fa15ef0
Abhishek Kumar <abhishek1706@hotmail.com>
2017-12-26 01:09:32 +0530

    banner , code for app_token , sdk added

--- app/src/main/java/com/buyhatke/autocart/Activity/MainActivity.java
+++ app/src/main/java/com/buyhatke/autocart/Activity/MainActivity.java
@@ -1,18 +1,23 @@
 package com.buyhatke.autocart.Activity;
 
+import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.ProgressDialog;
 import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.os.Handler;
+import android.os.Looper;
 import android.provider.Settings;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.LocalBroadcastManager;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -25,29 +30,39 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Interpolator;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.Scroller;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.DefaultRetryPolicy;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.buyhatke.assistant.Buyhatke;
 import com.buyhatke.autocart.AutoCart;
 import com.buyhatke.autocart.Constants;
 import com.buyhatke.autocart.R;
-import com.buyhatke.autocart.SaleAdapter;
+import com.buyhatke.autocart.Adapter.SaleAdapter;
 import com.buyhatke.autocart.SaleItem;
+import com.buyhatke.autocart.Adapter.FetchBannerAdapter;
 import com.google.android.gms.analytics.HitBuilders;
 import com.google.android.gms.analytics.Tracker;
+import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.gson.Gson;
 
 import org.json.JSONArray;
 import org.json.JSONException;
+
+import java.lang.reflect.Field;
+
+import me.relex.circleindicator.CircleIndicator;
 
 import static com.buyhatke.autocart.Constants.FEEDBACK_URL;
 import static com.buyhatke.autocart.Constants.SALE_URL;
@@ -65,10 +80,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private static final String REQUEST_TAG = "downloadSaleData";
     private BroadcastReceiver broadcastReceiver;
     private SharedPreferences sharedPref;
-    private static final String IS_REGISTERED = "isRegistered";
+    private static final String IS_REGISTERED = "isRegisteredAgain";
     private static final String REGISTER_TAG = "Registration";
     private static final String APP_OPEN_COUNT = "appOpenCount";
+    private static final String APP_OPEN_COUNT_SDK = "appOpenCountSDK";
     private static final String REVIEW_DONE = "reviewDone";
+    private static final String SDK_NOTIFICATION = "SDKNotification";
+    private long BANNER_TIME_DELAY = 7 * 1000;
+    private int BANNER_TRANSITION_DELAY = 1 * 1200;
+    private ViewPager pager;
+    private Handler handler;
+    private Runnable runnable;
+    private CircleIndicator circleIndicator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -93,6 +116,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         tvInstructionText = (TextView) findViewById(R.id.instrucion_text);
         ivInstruction = (ImageView) findViewById(R.id.iv_instr_arrow);
         llInstructionContainer = (LinearLayout) findViewById(R.id.ll_instruction_container);
+        circleIndicator = (CircleIndicator) findViewById(R.id.circular_indicator_home_pager);
+        pager = (ViewPager) findViewById(R.id.pager);
 
         tvMiSaleInfo.setOnClickListener(this);
         tvFlipkartInfo.setOnClickListener(this);
@@ -121,9 +146,72 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 }
             }
         };
-        if (!sharedPref.getBoolean(REVIEW_DONE, false))
-            checkAppOpenCount();
-        setupRecyclerView();
+
+    }
+
+    public void fetchBanner() {
+        String appId = sharedPref.getString("appId","");
+        String appAuth = sharedPref.getString("appAuth","");
+        String uri = Uri.parse("http://buyhatke.com/application/sendOfferNew2.php")
+                .buildUpon().appendQueryParameter("app_id", appId)
+                .appendQueryParameter("app_auth", appAuth).build().toString();
+
+        JsonArrayRequest jsArrayRequest = new JsonArrayRequest(uri,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        try {
+                            FetchBannerAdapter adapter = new FetchBannerAdapter(MainActivity.this, response);
+                            pager.setAdapter(adapter);
+                            pager.setVisibility(View.VISIBLE);
+
+                            circleIndicator.setViewPager(pager);
+                            adapter.registerDataSetObserver(circleIndicator.getDataSetObserver());
+
+                            try{
+                                Field mScroller = ViewPager.class.getDeclaredField("mScroller");
+                                mScroller.setAccessible(true);
+                                mScroller.set(pager, new CustomScroller(pager.getContext(), BANNER_TRANSITION_DELAY));
+                            } catch (Exception e){
+                            }
+
+                            handler = new Handler(Looper.getMainLooper());
+                            final int num = response.length();
+                            runnable = new Runnable() {
+                                public void run() {
+                                    int current = pager.getCurrentItem();
+                                    if(current >= num - 1)
+                                        current = -1;
+                                    pager.setCurrentItem(++current, true);
+                                }
+                            };
+
+                            //h2 = new Handler();
+                            handler.postDelayed(runnable, BANNER_TIME_DELAY);
+
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+
+                }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                fetchBanner();
+                Log.e("Banner", "Error response:" +
+                        error.getMessage());
+
+            }
+        });
+
+        jsArrayRequest.setRetryPolicy(new DefaultRetryPolicy(5000,
+                10,
+                DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+        jsArrayRequest.setShouldCache(true);
+        jsArrayRequest.setTag(this);
+        queue.add(jsArrayRequest);
     }
 
     private void checkAppOpenCount() {
@@ -133,6 +221,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         } else if (count < 5){
             count++;
             sharedPref.edit().putInt(APP_OPEN_COUNT, count).apply();
+        }
+    }
+
+    private void checkAppOpenCountSDK() {
+        int count = sharedPref.getInt(APP_OPEN_COUNT_SDK,0);
+        if (count == 13){
+            showSDKDialog();
+        } else if (count < 13){
+            count++;
+            sharedPref.edit().putInt(APP_OPEN_COUNT_SDK, count).apply();
         }
     }
 
@@ -202,7 +300,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     saleItems = gson.fromJson(jsonArray.getString(2), SaleItem[].class);
                     rvAmazon.setAdapter(new SaleAdapter(saleItems, MainActivity.this, SaleAdapter.SITE_AMAZON));
                     if (!sharedPref.getBoolean(IS_REGISTERED, false)) {
-                        registerApp();
+                        getDeviceId();
                     }
                 } catch (JSONException e) {
                     e.printStackTrace();
@@ -300,6 +398,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         // by doing this, the activity will be notified each time a new message arrives
         LocalBroadcastManager.getInstance(this).registerReceiver(broadcastReceiver,
                 new IntentFilter(Constants.PUSH_NOTIFICATION));
+        setupRecyclerView();
+        fetchBanner();
+        if (!sharedPref.getBoolean(REVIEW_DONE, false))
+            checkAppOpenCount();
+        if (!sharedPref.getBoolean(SDK_NOTIFICATION, false))
+            checkAppOpenCountSDK();
     }
 
     @Override
@@ -308,8 +412,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             if (grantResults.length > 0 &&
                     grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 Log.d(REGISTER_TAG, "Permission granted");
-                registerApp();
+                registerApp(getDeviceId());
+            } else {
+                registerApp("");
             }
+            showSDKDialog();
         }
     }
 
@@ -317,7 +424,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         TelephonyManager telephonyManager = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
         if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.READ_PHONE_STATE},101);
-            return null;
+            return "";
         }
         else if (telephonyManager.getDeviceId() != null)
             return telephonyManager.getDeviceId();
@@ -326,10 +433,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     Settings.Secure.ANDROID_ID);
     }
 
-    private void sendRegisterIdToBackend(final String appId, final String appAuth) {
+    private void sendRegisterIdToBackend(final String appId, final String appAuth, final String appToken) {
         final String setTokenUrl = Constants.SET_TOKEN_URL
+                + "&app_token=" + appToken
                 + "&app_id=" + appId
                 + "&app_auth=" + appAuth;
+        Log.d("Important",setTokenUrl);
         StringRequest request = new StringRequest(setTokenUrl, new Response.Listener<String>() {
             @Override
             public void onResponse(String response) {
@@ -340,13 +449,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 }
                 else {
                     Log.d(REGISTER_TAG, "Invalid Response Recieved. Trying again");
-                    sendRegisterIdToBackend(appId, appAuth);
+                    sendRegisterIdToBackend(appId, appAuth, appToken);
                 }
             }
         }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-                sendRegisterIdToBackend(appId, appAuth);
+                sendRegisterIdToBackend(appId, appAuth, appToken);
                 Log.d(REGISTER_TAG, "Token set failed. Trying again");
             }
         });
@@ -354,35 +463,102 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         queue.add(request);
     }
 
-    private void registerApp() {
-        String idPhone = getDeviceId();
-        if (!TextUtils.isEmpty(idPhone)){
-            String url = Uri.parse(Constants.REGISTER_URL).buildUpon()
-                    .appendQueryParameter("source", "autocart_app")
-                    .appendQueryParameter("imei", idPhone).toString();
+    private void registerApp(final String idPhone) {
+        final String appToken = FirebaseInstanceId.getInstance().getToken();
+        String url = Uri.parse(Constants.REGISTER_URL).buildUpon()
+                .appendQueryParameter("source", "autocart_app")
+                .appendQueryParameter("imei", idPhone).toString();
 
-            JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(url, new Response.Listener<JSONArray>() {
-                @Override
-                public void onResponse(JSONArray response) {
-                    try {
-                        String appId = response.getJSONObject(0).getString("app_id");
-                        String appAuth = response.getJSONObject(0).getString("app_auth");
-                        Log.d(REGISTER_TAG, appId + " " + appAuth);
-                        sendRegisterIdToBackend(appId, appAuth);
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                        Log.d(REGISTER_TAG, "App registration failed. Trying again.");
-                    }
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(url, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+                try {
+                    String appId = response.getJSONObject(0).getString("app_id");
+                    String appAuth = response.getJSONObject(0).getString("app_auth");
+                    Log.d(REGISTER_TAG, appId + " " + appAuth);
+                    sendRegisterIdToBackend(appId, appAuth, appToken);
+                    SharedPreferences.Editor editor = sharedPref.edit();
+                    editor.putString("appId",appId);
+                    editor.putString("appAuth",appAuth);
+                    editor.putString("appToken",appToken);
+                    editor.apply();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    Log.d(REGISTER_TAG, "App registration failed. Trying again.");
                 }
-            }, new Response.ErrorListener() {
-                @Override
-                public void onErrorResponse(VolleyError error) {
-                    registerApp();
-                }
-            });
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                registerApp(idPhone);
+            }
+        });
 
-            jsonArrayRequest.setTag(REGISTER_TAG);
-            queue.add(jsonArrayRequest);
+        jsonArrayRequest.setTag(REGISTER_TAG);
+        queue.add(jsonArrayRequest);
+    }
+
+    private class CustomScroller extends Scroller {
+
+        private int mDuration;
+
+        public CustomScroller(Context context, int mDuration) {
+            super(context);
+            this.mDuration = mDuration;
+        }
+
+        public CustomScroller(Context context, Interpolator interpolator, int mDuration) {
+            super(context, interpolator);
+            this.mDuration = mDuration;
+        }
+
+        public CustomScroller(Context context, Interpolator interpolator, boolean flywheel, int mDuration) {
+            super(context, interpolator, flywheel);
+            this.mDuration = mDuration;
+        }
+
+        @Override
+        public void startScroll(int startX, int startY, int dx, int dy) {
+            super.startScroll(startX, startY, dx, dy, mDuration);
+        }
+
+        @Override
+        public void startScroll(int startX, int startY, int dx, int dy, int duration) {
+            super.startScroll(startX, startY, dx, dy, mDuration);
         }
     }
+
+    private void showSDKDialog(){
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Enable Shopping Assistant?");
+        builder.setMessage("Let Buyhatke Shopping Assistant give suggestions and help save more whenever you surf on your favourite shopping app?");
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                if (!Buyhatke.isAccessibilityServiceRunning(MainActivity.this))
+                    Buyhatke.setAccessibilityNotification(MainActivity.this);
+                builder.show().dismiss();
+                Toast.makeText(MainActivity.this, "Click on the notification for further steps!", Toast.LENGTH_LONG).show();
+                sharedPref.edit().putBoolean(SDK_NOTIFICATION, true).apply();
+                AutoCart.sendUpdateToServer("SDK", "Yes");
+            }
+        });
+        builder.setNegativeButton("Later", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                builder.show().dismiss();
+                sharedPref.edit().putInt(APP_OPEN_COUNT_SDK, 0).apply();
+                AutoCart.sendUpdateToServer("SDK","Later");
+            }
+        });
+        builder.setNeutralButton("Never", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                builder.show().dismiss();
+                AutoCart.sendUpdateToServer("SDK", "Never");
+            }
+        });
+        builder.show();
+    }
+
 }
